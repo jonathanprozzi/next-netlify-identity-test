80294016bcceba0e637b7290f8522eb4fc98d1b0
jonathanprozzi <jonathanprozzi@gmail.com>
2021-02-15 20:59:40 -0500

    testing out deploy

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import netlifyAuth from '../netlifyAuth';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
+
+  useEffect(() => {
+    let isCurrent = true;
+    netlifyAuth.initialize((user) => {
+      if (isCurrent) {
+        setLoggedIn(user);
+      }
+    });
+
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
+  };
+
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+  };
+
   return (
     <div className="container">
       <Head>
@@ -11,13 +44,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {loggedIn ? (
+          <Header title="you were ZeroCool?" />
+        ) : (
+          <>
+            <Header title="pls hack ur way into the gibson first" />
+            <button onClick={login}>engage hacking</button>
+          </>
+        )}
       </main>
-
       <Footer />
     </div>
-  )
+  );
 }
